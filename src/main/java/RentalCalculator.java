9024042d40014887c9584233251c3083bbd85f7b
darksyaf <33533564+darksyaf@users.noreply.github.com>
2018-02-02 09:24:53 +0800

    Add files via upload

---
+++ src/main/java/RentalCalculator.java
@@ -0,0 +1,53 @@
+
+public class RentalCalculator {
+
+	public static double carRental(String carModel, int duration, boolean firstTimer, boolean member){
+		
+		double rentalFees = 0.0;
+				
+		if (firstTimer){
+			if (carModel.equals("Honda"))
+				rentalFees = 65.0*duration*0.95;
+			else
+				rentalFees = 80.0*duration*0.95;
+		}
+		else {
+			if (member){
+
+				//handle member transactions
+				// ...
+				
+				if (carModel.equals("Honda") && (duration < 3))
+					rentalFees = 65.0*duration*0.90;
+				
+				else if (carModel.equals("Honda") && (duration >= 3))
+					rentalFees = 65.0*duration*0.80;
+				
+				else if (carModel.equals("Toyota") && (duration < 3))
+					rentalFees = 80.0*duration*0.90;
+				
+				else
+					rentalFees = 80.0*duration*0.80;
+					
+				
+
+			}
+			else { //non-member
+				
+				//handle non-member transactions
+				//...
+				
+				if (carModel.equals("Honda"))
+					rentalFees = 65.0*duration;
+				
+				else 
+					rentalFees = 80.0*duration;
+				
+			}
+		}
+		
+		return rentalFees;
+	}
+	
+
+}
